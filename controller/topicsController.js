7e91084c6ea4f9d3fe7339ad332de38a6bc3a3e8
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2019-01-29 10:39:54 +0100

    initial commit

---
+++ controller/topicsController.js
@@ -0,0 +1,22 @@
+var exports = module.exports = {};
+
+
+exports.list_topics = function(req, res) {
+
+};
+
+exports.list_topic = function(req, res) {
+
+};
+
+exports.list_topic_mid = function(req, res) {
+
+};
+
+exports.create_topic = function(req, res) {
+
+};
+
+exports.create_topic_msg = function(req, res) {
+
+};
